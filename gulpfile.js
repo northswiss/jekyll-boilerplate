c81f35523338d956691852fdb1ffb1374ccefcc0
Chris Roy <chris@northswiss.com>
2015-09-25 00:03:46 +0100

    - Added custom JS condition to footer.html - Disabled browser sync pop up - Added new navigation format. Leaner version

--- gulpfile.js
+++ gulpfile.js
@@ -5,7 +5,7 @@ var browserSync = require('browser-sync').create();
 gulp.task('build', shell.task(['jekyll build --watch']));
 
 gulp.task('serve', function () {
-    browserSync.init({server: {baseDir: '_site/'}});
+    browserSync.init({server: {baseDir: '_site/'}, notify: false});
     // Reloads page when some of the already built files changed:
     gulp.watch('_site/**/*.*').on('change', browserSync.reload);
 });
