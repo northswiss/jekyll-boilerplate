19069e4e67865e2e3d46e3559bc5598f09c76379
Chris Roy <chris@northswiss.com>
2016-01-06 08:46:55 +0000

    Restructured homepage into components and additional content changes

--- js/main.js
+++ js/main.js
@@ -1,12 +1,8 @@
+var body = $("body");
+
+
 $("#more").click(function() {
   $('html, body').animate({
     scrollTop: $("#problem").offset().top-6
   }, 1000);
 });
-
-var budgetSection = $("#budget");
-console.log(budgetSection.offset.top);
-
-if ( budgetSection.y = 0 ) {
-  console.log("OK");
-}
