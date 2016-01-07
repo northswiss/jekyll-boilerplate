c498e9a31a2c2991954649aa395d5016974b7b70
Chris Roy <chris@travelperk.com>
2016-01-07 16:55:47 +0000

    Added rough outline for travel bookers and travelers as tabbed section

--- js/main.js
+++ js/main.js
@@ -1,8 +1,34 @@
-var body = $("body");
-
+var body = $("body"),
+    site_header =           $("#header"),
+    btn_booker =            $("#btn_booker"),
+    btn_traveler =          $("#btn_traveler"),
+    btn_manager =           $("#btn_manager"),
+    slide_wrapper =         $("#slide_wrapper");
 
 $("#more").click(function() {
   $('html, body').animate({
     scrollTop: $("#problem").offset().top-6
   }, 1000);
 });
+
+$(window).scroll(function () {
+  if( $(this).scrollTop() > site_header.offset().top) {
+    site_header.addClass("fixed");
+  } else {
+    site_header.removeClass("fixed");
+  }
+});
+
+
+// SLIDES
+
+btn_booker.click(function () {
+  slide_wrapper.attr("data-state", "slide-one");
+});
+btn_traveler.click(function () {
+  slide_wrapper.attr("data-state", "slide-two");
+});
+btn_manager.click(function () {
+  slide_wrapper.attr("data-state", "slide-three");
+});
+
