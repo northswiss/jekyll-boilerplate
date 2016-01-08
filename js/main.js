e85add41db85d4723639d3dbf252e85da4d075af
Chris Roy <chris@travelperk.com>
2016-01-08 14:22:53 +0000

    Lots of stuff - before first meeting

--- js/main.js
+++ js/main.js
@@ -3,11 +3,15 @@ var body = $("body"),
     btn_booker =            $("#btn_booker"),
     btn_traveler =          $("#btn_traveler"),
     btn_manager =           $("#btn_manager"),
-    slide_wrapper =         $("#slide_wrapper");
+    btn_signup =            $("#signup"),
+    btn_close =             $("#close"),
+    slide_wrapper =         $("#slide_wrapper"),
+    overlay =               $("#overlay"),
+    popup_signup =          $("#popup_signup");
 
 $("#more").click(function() {
   $('html, body').animate({
-    scrollTop: $("#problem").offset().top-6
+    scrollTop: $("#customer_types").offset().top-93
   }, 1000);
 });
 
@@ -24,11 +28,38 @@ $(window).scroll(function () {
 
 btn_booker.click(function () {
   slide_wrapper.attr("data-state", "slide-one");
+  btn_traveler.removeClass("active");
+  btn_manager.removeClass("active");
+  $(this).addClass("active");
 });
 btn_traveler.click(function () {
   slide_wrapper.attr("data-state", "slide-two");
+  btn_booker.removeClass("active");
+  btn_manager.removeClass("active");
+  $(this).addClass("active");
 });
 btn_manager.click(function () {
   slide_wrapper.attr("data-state", "slide-three");
+  btn_booker.removeClass("active");
+  btn_traveler.removeClass("active");
+  $(this).addClass("active");
 });
 
+// OVERLAY
+
+btn_signup.on( 'click', function () {
+    showPopup();
+  }
+);
+
+btn_close.click(closePopUp);
+
+function showPopup ( whichPopup ) {
+  overlay.fadeIn("fast");
+}
+
+function closePopUp(e) {
+  console.log("closePopup called");
+  e.preventDefault();
+  overlay.fadeOut("fast");
+}
